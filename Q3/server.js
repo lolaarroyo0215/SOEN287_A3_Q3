2582eff8ae19f0426f13418bd79f2a61e4a62c62
lolaarroyo0215 <146749932+lolaarroyo0215@users.noreply.github.com>
2024-08-13 17:40:09 -0400

    Update server.js

--- Q3/server.js
+++ Q3/server.js
@@ -1,26 +1,21 @@
-import http from "http";
-import fs from 'fs/promises';
-import url from 'url';
-import path from 'path';
-import { phoneChecker } from './phoneChecker.js';
+const http = require('http');
+const fs = require('fs').promises;
+const url = require('url');
+const path = require('path');
+const { phoneChecker } = require('./phoneChecker.js');
 
 const PORT = process.env.PORT || 8000;
 
-//Get current path
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__filename, __dirname);
-
 const server = http.createServer(async (req, res) => {
-    try{
-        console.log(`Received request: ${req.method} ${req.url}`)
-        //check if GET request
-        if(req.method === 'GET'){
+    try {
+        console.log(`Received request: ${req.method} ${req.url}`);
+        // Check if GET request
+        if (req.method === 'GET') {
 
-            const parsedUrl = new URL(req.url, `https://${req.headers.host}`);
+            const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
             console.log(`Request URL: ${req.url}`);
 
-            if(parsedUrl.pathname === '/' || parsedUrl.pathname === '/index-phone.html') {
+            if (parsedUrl.pathname === '/' || parsedUrl.pathname === '/index-phone.html') {
                 const filePath = path.join(__dirname, 'public3', 'index-phone.html');
                 console.log(`Serving file: ${filePath}`);
                 const data = await fs.readFile(filePath);
@@ -36,7 +31,7 @@ const server = http.createServer(async (req, res) => {
                 res.end();
             } else if (parsedUrl.pathname === '/index-phone') {
                 // Redirect to the form without query parameters
-                console.log('Redirecting to /index.phone.html');
+                console.log('Redirecting to /index-phone.html');
                 res.writeHead(302, { Location: '/index-phone.html' });
                 res.end();
             } else {
@@ -46,12 +41,12 @@ const server = http.createServer(async (req, res) => {
             throw new Error('Method not allowed');
         }
 
-    } catch (error){
+    } catch (error) {
         console.error('Server error: ', error.message);
-        if(!res.headersSent){
-            res.writeHead(500, {'Content-Type' : 'text/plain' });
+        if (!res.headersSent) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
         }
-        res.end(`Server error:' ${error.message}`);
+        res.end(`Server error: ${error.message}`);
     }
 });
 
