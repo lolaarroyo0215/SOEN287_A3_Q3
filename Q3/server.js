21bd18efb9d59577306d5d6a91c7021415502bae
lolaarroyo0215 <146749932+lolaarroyo0215@users.noreply.github.com>
2024-08-13 17:47:01 -0400

    Update server.js

--- Q3/server.js
+++ Q3/server.js
@@ -2,9 +2,20 @@ const http = require('http');
 const fs = require('fs').promises;
 const url = require('url');
 const path = require('path');
-const { phoneChecker } = require('./phoneChecker.js');
 
 const PORT = process.env.PORT || 8000;
+
+
+function phoneChecker(phoneNumber){
+    const regex = /^\d{3}-\d{3}-\d{4}$/;
+    let message = '';
+    if(regex.test(phoneNumber)){
+        message = 'phone number is valid';
+    } else {
+        message = `phone number: ${phoneNumber} is not valid\nit should match the format: ddd-ddd-dddd`;
+    }
+    return message;
+}
 
 const server = http.createServer(async (req, res) => {
     try {
